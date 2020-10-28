399d7ce66a7c381ad0ea66a0d640b50894fc068d
NISHANT JHA <nishant.jhaa@outlook.com>
2020-10-29 00:39:26 +0530

    Add files via upload

--- styles.js
+++ styles.js
@@ -1,38 +1,21 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles({
-  media: {
-    height: 250,
-  },
-  border: {
-    border: 'solid',
-  },
-  fullHeightCard: {
-    height: '50%',
-  },
   card: {
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'space-between',
-    borderBottom: '10px solid white',
+    alignItems: 'center',
+    width: '100%',
+    height: '50vh',
+    padding: '5%',
+    borderRadius: 10,
+    color: 'white',
   },
-  activeCard: {
-    borderBottom: '12px solid red',
+  infoCard: {
+    display: 'flex', flexDirection: 'column', textAlign: 'center',
   },
-  grid: {
-    display: 'flex',
-  },
-  details: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    margin: '20px',
-  },
-  title: {
-    padding: '0 16px',
-  },
-  cardActions: {
-    padding: '0 16px 8px 16px',
-    display: 'flex',
-    justifyContent: 'space-between',
+  container: {
+    padding: '0 5%', width: '100%', margin: 0,
   },
 });
